17ee12be5c660a1bf48720af101dddbc9d40beeb
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2023-10-15 22:18:33 +0200

    installed and configured firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZmABv7taPjSETZmYoqHIW4sez_vO8JZk",
+  authDomain: "healers-d5b16.firebaseapp.com",
+  projectId: "healers-d5b16",
+  storageBucket: "healers-d5b16.appspot.com",
+  messagingSenderId: "933483465262",
+  appId: "1:933483465262:web:fafb44644351d854b82dbf",
+  measurementId: "G-N777PS1LPP"
+};
+
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
